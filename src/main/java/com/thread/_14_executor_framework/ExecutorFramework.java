85b7d119a837d8f0845e7bcd46889a00e91bc2e8
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-01-02 09:31:29 +0530

    Executor framework theory

---
+++ src/main/java/com/thread/_14_executor_framework/ExecutorFramework.java
@@ -0,0 +1,17 @@
+package com.thread._14_executor_framework;
+
+/**
+ * The Executor framework introduced in java 5
+ * as part of java.util.concurrent package to simplify
+ * the development of concurrent applications to abstracting away many
+ * of the complexity involved in creating and managing threads.
+ *
+ * Problem solved by Executor framework.
+ * 1. Manual Thread Management.
+ * 2. Resource Management.
+ * 3. Scalability.
+ * 4. Thread Reuse.
+ * 5. Error handling.
+ * */
+public interface ExecutorFramework {
+}
