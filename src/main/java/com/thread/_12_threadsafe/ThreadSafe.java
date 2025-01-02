f5dc7e15ab762075551872baba1d1c332246028b
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-01-02 06:35:47 +0530

    add thread safe example

---
+++ src/main/java/com/thread/_12_threadsafe/ThreadSafe.java
@@ -0,0 +1,8 @@
+package com.thread._12_threadsafe;
+
+/**
+ * To make the code thread-safe, you need to ensure that shared data is accessed in a controlled manner
+ * to avoid race conditions or inconsistent states/result
+ * */
+public interface ThreadSafe {
+}
