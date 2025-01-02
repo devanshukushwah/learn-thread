511a9933672abd801aa3b04d768ac249299037e4
Devanshu Kushwah <devanshukushwah.001@gmail.com>
2025-01-02 09:31:07 +0530

    thread pool

---
+++ src/main/java/com/thread/_13_threadpool/ThreadPool.java
@@ -0,0 +1,12 @@
+package com.thread._13_threadpool;
+
+/**
+ * Set of pre-initialized thread is called thread pool.
+ *
+ * why?
+ * 1. Resource Management.
+ * 2. Response Time.
+ * 3. Control over Thread count.
+ * */
+public interface ThreadPool {
+}
